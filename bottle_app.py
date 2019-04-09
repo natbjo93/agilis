77d66b138ba1b6333ad4a3498780e08983c487d3
nathalie <nathaliebjornsson@hotmail.com>
2019-04-09 08:48:50 +0200

    Initial commit

---
+++ bottle_app.py
@@ -0,0 +1,11 @@
+from bottle import route, run, template, static_file
+
+@route("/")
+def index():
+    return template("index", root="static")
+
+@route("/static/<filename>")
+def server_static(filename):
+    return static_file(filename, root="static")
+
+run(host="localhost", port=8000)
