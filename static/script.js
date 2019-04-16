a5815e5a2ff0eeec2b0e8c209eba027e7ff26b55
ronjanackblad <ronja.nackblad@hotmail.com>
2019-04-16 23:28:17 +0200

    merge will add register and css style

---
+++ static/script.js
@@ -0,0 +1,17 @@
+function asterixPassword() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+function asterixPassword2() {
+  var x = document.getElementById("password2");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
