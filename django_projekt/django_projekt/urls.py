852d2fe330ee81d5f8bb414c3386ad30b79d1338
ronjanackblad <ronja.nackblad@hotmail.com>
2019-05-13 10:32:43 +0200

    django login

--- django_projekt/django_projekt/urls.py
+++ django_projekt/django_projekt/urls.py
@@ -21,4 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('accounts/', include('allauth.urls')),
+    
 ]
