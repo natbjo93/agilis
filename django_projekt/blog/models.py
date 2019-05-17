6f8d012f6e282fe516b2ab0ddc987cfdd8a4b990
petrasvensosn10 <petra.svensson@live.se>
2019-05-17 10:25:56 +0200

    strukturerat filer

--- django_projekt/blog/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class CustomUser(AbstractUser):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.email
